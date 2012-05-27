22fc838f6c5e61b1015cf88d9060b44cd2ad93a0
Charles Munger <charlesmunger@(none).(none)>
2012-05-26 17:14:54 -0700

    derp

--- jPregel-source-2-28-2012/JpAws/Machine.java
+++ jPregel-source-2-28-2012/JpAws/Machine.java
@@ -1,17 +1,28 @@
 package JpAws;
 
-
-
-import java.io.IOException;
 import com.xerox.amazonws.ec2.EC2Exception;
+import java.io.IOException;
 
+/**
+ *
+ * @author charlesmunger
+ */
 abstract public class Machine {
-	
-	abstract public String[] start(int numWorkers, String imageId) throws EC2Exception, IOException ; 
-	
-	abstract public void Stop() throws EC2Exception, IOException ; 
-		
-	}
-	
 
+    /**
+     *  Starts this machine, with n workers. 
+     * @param numWorkers Specifies the number of worker threads. 
+     * @param imageId Specifies the UID of the system 
+     * @return
+     * @throws EC2Exception
+     * @throws IOException
+     */
+    abstract public String[] start(int numWorkers, String imageId) throws EC2Exception, IOException;
 
+    /**
+     * Stops the machine.
+     * @throws EC2Exception
+     * @throws IOException
+     */
+    abstract public void Stop() throws EC2Exception, IOException;
+}
