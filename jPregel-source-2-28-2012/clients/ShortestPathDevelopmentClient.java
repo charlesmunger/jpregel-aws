cd8eef8a33c6ee124e09fa96ccd60fd7138ad6fe
charlesmunger <charlesmunger@localhost>
2012-06-02 23:56:03 -0700

    Added run configuration for Dev client.

--- jPregel-source-2-28-2012/clients/ShortestPathDevelopmentClient.java
+++ jPregel-source-2-28-2012/clients/ShortestPathDevelopmentClient.java
@@ -2,21 +2,13 @@ package clients;
 
 import JpAws.Machine;
 import static java.lang.System.out;
-
 import java.rmi.RemoteException;
-import vertex.ShortestPathVertex;
 import masterGraphMakers.G1MasterGraphMaker;
 import masterOutputMakers.StandardMasterOutputMaker;
-import system.Client;
-import system.Combiner;
-import system.GraphMaker;
-import system.Job;
-import system.MasterGraphMaker;
-import system.Vertex;
-import system.WorkerWriter;
-import system.Writer;
+import system.*;
 import system.aggregators.IntegerSumAggregator;
 import system.combiners.IntegerMinCombiner;
+import vertex.ShortestPathVertex;
 import workerGraphMakers.StandardWorkerGraphMaker;
 import workerOutputMakers.StandardWorkerOutputMaker;
 
@@ -24,22 +16,20 @@ import workerOutputMakers.StandardWorkerOutputMaker;
  *
  * @author Pete Cappello
  */
-public class ShortestPathDevelopmentClient
-{
+public class ShortestPathDevelopmentClient {
+
     /**
      * @param args the command line arguments
      */
-    public static void main( String[] args ) throws RemoteException
-    {
-        String  jobName               = "Shortest Path Problem";
-        String  jobDirectoryName      = args[0];
-        int     numWorkers            = Integer.parseInt(     args[1] );
-        boolean workerIsMultithreaded = Boolean.parseBoolean( args[2] );
-        boolean combiningMessages     = Boolean.parseBoolean( args[3] );
-        int     numParts = numWorkers * 2 * 2; // numWorkers * ComputeThrads/Worker * Parts/ComputeThread
+    public static void main(String[] args) throws RemoteException {
+        String jobName = "Shortest Path Problem";
+        String jobDirectoryName = args[0];
+        int numWorkers = Integer.parseInt(args[1]);
+        boolean workerIsMultithreaded = Boolean.parseBoolean(args[2]);
+        boolean combiningMessages = Boolean.parseBoolean(args[3]);
+        int numParts = numWorkers * 2 * 2; // numWorkers * ComputeThreads/Worker * Parts/ComputeThread
         Combiner combiner = null;
-        if ( combiningMessages )
-        {
+        if (combiningMessages) {
             combiner = new IntegerMinCombiner();
         }
         Vertex vertexFactory = new ShortestPathVertex();
@@ -47,32 +37,28 @@ public class ShortestPathDevelopmentClient
         GraphMaker workerGraphMaker = new StandardWorkerGraphMaker();
         MasterGraphMaker reader = new G1MasterGraphMaker();
         Writer writer = new StandardMasterOutputMaker();
-        
+
         out.println("ShortestPathDevelopmentClient.main: "
                 + "\n jobDirectoryName: " + jobDirectoryName
                 + "\n numParts: " + numParts
                 + "\n numWorkers: " + numWorkers
                 + "\n workerIsMultithreaded: " + workerIsMultithreaded
-                + "\n combining messages: " + combiningMessages
-                );
-        
-        Job job = new Job( jobName,
-                jobDirectoryName, 
-                vertexFactory, numParts, workerIsMultithreaded, combiner, workerWriter, 
-                workerGraphMaker, reader, writer );                
-        job.setProblemAggregator( new IntegerSumAggregator() );
-        job.setStepAggregator(    new IntegerSumAggregator() );
-        Job[] jobs = { job };
-        try
-        {
-            boolean   isEc2Master = false;
-            Client.run( jobs, isEc2Master, numWorkers ,Machine.AMIID,Machine.AMIID);//TODO fix this
-        }
-        catch( Exception exception )
-        {
+                + "\n combining messages: " + combiningMessages);
+
+        Job job = new Job(jobName,
+                jobDirectoryName,
+                vertexFactory, numParts, workerIsMultithreaded, combiner, workerWriter,
+                workerGraphMaker, reader, writer);
+        job.setProblemAggregator(new IntegerSumAggregator());
+        job.setStepAggregator(new IntegerSumAggregator());
+        Job[] jobs = {job};
+        try {
+            boolean isEc2Master = false;
+            Client.run(jobs, isEc2Master, numWorkers, Machine.AMIID, Machine.AMIID);//TODO fix this
+        } catch (Exception exception) {
             exception.printStackTrace();
-            System.exit( 1 );
+            System.exit(1);
         }
-        System.exit( 0 );
+        System.exit(0);
     }
- }
+}
