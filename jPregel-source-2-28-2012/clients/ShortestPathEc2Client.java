48bdc6ffe07a430f7cda993c16c8059ae1b1f78e
Charles Munger <charleslmunger@gmail.com>
2012-06-11 23:19:33 -0700

    Exited system to return control to user sooner.

--- jPregel-source-2-28-2012/clients/ShortestPathEc2Client.java
+++ jPregel-source-2-28-2012/clients/ShortestPathEc2Client.java
@@ -1,19 +1,11 @@
 package clients;
 
-import JpAws.Machine;
 import java.rmi.RemoteException;
-import vertex.ShortestPathVertex;
 import masterGraphMakers.G1MasterGraphMaker;
 import masterOutputMakers.StandardMasterOutputMaker;
-import system.Client;
-import system.Combiner;
-import system.GraphMaker;
-import system.Job;
-import system.MasterGraphMaker;
-import system.Vertex;
-import system.WorkerWriter;
-import system.Writer;
+import system.*;
 import system.combiners.IntegerMinCombiner;
+import vertex.ShortestPathVertex;
 import workerGraphMakers.StandardWorkerGraphMaker;
 import workerOutputMakers.StandardWorkerOutputMaker;
 
@@ -21,23 +13,21 @@ import workerOutputMakers.StandardWorkerOutputMaker;
  *
  * @author Pete Cappello
  */
-public class ShortestPathEc2Client extends Client
-{
+public class ShortestPathEc2Client extends Client {
+
     /**
      * @param args the command line arguments
      */
-    public static void main( String[] args ) throws RemoteException
-    {
-        String    jobName               = "ShortestPath";
-        String    jobDirectoryName      = args[0];
-        int       numParts              = Integer.parseInt( args[1] );
-        int       numWorkers            = Integer.parseInt( args[2] );
-        boolean   workerIsMultithreaded = Boolean.parseBoolean( args[3]);
-        boolean   isEc2Master           = true;
-        boolean combiningMessages       = Boolean.parseBoolean( args[4]);; 
+    public static void main(String[] args) throws RemoteException {
+        String jobName = "ShortestPath";
+        String jobDirectoryName = args[0];
+        int numParts = Integer.parseInt(args[1]);
+        int numWorkers = Integer.parseInt(args[2]);
+        boolean workerIsMultithreaded = Boolean.parseBoolean(args[3]);
+        boolean isEc2Master = true;
+        boolean combiningMessages = Boolean.parseBoolean(args[4]);;        
         Combiner combiner = null;
-        if ( combiningMessages )
-        {
+        if (combiningMessages) {
             combiner = new IntegerMinCombiner();
         }
         Vertex vertexFactory = new ShortestPathVertex();
@@ -45,11 +35,12 @@ public class ShortestPathEc2Client extends Client
         GraphMaker workerGraphMaker = new StandardWorkerGraphMaker();
         MasterGraphMaker reader = new G1MasterGraphMaker();
         Writer writer = new StandardMasterOutputMaker(jobDirectoryName);
-        
-        Job job = new Job( jobName,
-                jobDirectoryName, 
-                vertexFactory, numParts, workerIsMultithreaded, combiner, 
-                workerWriter, workerGraphMaker, reader, writer );
-        Job[] jobs = { job };
-        Client.run( jobs, isEc2Master, numWorkers);    }
+        Job job = new Job(jobName,
+                jobDirectoryName,
+                vertexFactory, numParts, workerIsMultithreaded, combiner,
+                workerWriter, workerGraphMaker, reader, writer);
+        Job[] jobs = {job};
+        Client.run(jobs, isEc2Master, numWorkers);        
+        System.exit(0);
+    }
 }
