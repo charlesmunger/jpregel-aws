438343f34050975db85373ed3347837c61abe015
Charles Munger <charleslmunger@gmail.com>
2012-06-18 23:34:25 -0700

    Added security group programmatically.

--- jPregel-source-2-28-2012/JpAws/MasterMachines.java
+++ jPregel-source-2-28-2012/JpAws/MasterMachines.java
@@ -1,10 +1,12 @@
 package JpAws;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.*;
 import datameer.awstasks.aws.ec2.InstanceGroupImpl;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -20,11 +22,25 @@ public class MasterMachines extends Ec2Machine {
     public String[] start(int numWorkers) throws IOException {
         
         instanceGroup = new InstanceGroupImpl(ec2);
-        DescribeSecurityGroupsResult describeSecurityGroups = ec2.describeSecurityGroups(new DescribeSecurityGroupsRequest().withGroupNames(Ec2Machine.SECURITY_GROUP));
-        System.out.println(describeSecurityGroups);
+        CreateSecurityGroupResult createSecurityGroup;
+        DescribeSecurityGroupsResult describeSecurityGroups = null;
+        try {
+             describeSecurityGroups = ec2.describeSecurityGroups(new DescribeSecurityGroupsRequest().withGroupNames(Ec2Machine.SECURITY_GROUP));
+        } catch(AmazonServiceException e) { 
+             createSecurityGroup = ec2.createSecurityGroup(new CreateSecurityGroupRequest(Ec2Machine.SECURITY_GROUP, "Created programatically by jpregel-aws"));
+        }
+        List<IpPermission> ipPermissions = describeSecurityGroups.getSecurityGroups().get(0).getIpPermissions();
+        IpPermission p = new IpPermission();
+        p.setIpProtocol("tcp");
+        p.setFromPort(0);
+        p.setToPort(65535);
+        p.setIpRanges(Collections.singleton("0.0.0.0/0"));
+        if(!ipPermissions.contains(p)) {
+            ec2.authorizeSecurityGroupIngress(new AuthorizeSecurityGroupIngressRequest(Ec2Machine.SECURITY_GROUP, Collections.singletonList(p)));
+        }
         RunInstancesRequest launchConfiguration = new RunInstancesRequest(Machine.AMIID, 1, 1)
                     .withKeyName(PregelAuthenticator.get().getMasterPrivateKeyName())
-                    .withInstanceType("t1.micro").withSecurityGroupIds("quick-start-1");
+                    .withInstanceType("t1.micro").withSecurityGroupIds(Ec2Machine.SECURITY_GROUP);
                     System.out.println(launchConfiguration.toString());
 
         Reservation rs = instanceGroup.launch(launchConfiguration, TimeUnit.MINUTES, 5);
