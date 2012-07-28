843a0518a42caed3fa4a8f21e31ae7cadedfeb64
Pete Cappello <cappello@cs.ucsb.edu>
2012-07-28 00:09:33 -0700

    SuperStepToActiveSetMap: Dropped in ConcurrentHashMap.

--- jPregel-source-2-28-2012/system/SuperStepToActiveSetMap.java
+++ jPregel-source-2-28-2012/system/SuperStepToActiveSetMap.java
@@ -1,30 +1,35 @@
 package system;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
  * @author Pete Cappello
  */
-public class SuperStepToActiveSetMap extends HashMap<Long,Set<Vertex>>
+public class SuperStepToActiveSetMap extends ConcurrentHashMap<Long,Set<Vertex>>
 {
-    synchronized public Set<Vertex> get( long superStep )
+//    synchronized public Set<Vertex> get( long superStep )
+    public Set<Vertex> get( long superStep )
     {
-        Set<Vertex> activeSet = super.get( superStep );
-        if ( activeSet == null )
-        {
-            activeSet = new HashSet<Vertex>();
-            super.put( superStep, activeSet );
-        }
-        return activeSet;
+        putIfAbsent( superStep, new HashSet<Vertex>() );
+//        Set<Vertex> activeSet = super.get( superStep );
+//        if ( activeSet == null )
+//        {
+//            activeSet = new HashSet<Vertex>();
+//            super.put( superStep, activeSet );
+//        }
+//        return activeSet;
+        return super.get( superStep );
     }
     
-    synchronized public Set<Vertex> put( Long superStep, Set<Vertex> activeSet )
+//    synchronized public Set<Vertex> put( Long superStep, Set<Vertex> activeSet )
+    public Set<Vertex> put( Long superStep, Set<Vertex> activeSet )
     {
         return super.put( superStep, activeSet );
     }
     
-    synchronized public Set<Vertex> remove( Long superStep ) { return super.remove( superStep ); }
+//    synchronized public Set<Vertex> remove( Long superStep ) { return super.remove( superStep ); }
+    public Set<Vertex> remove( Long superStep ) { return super.remove( superStep ); }
 }
