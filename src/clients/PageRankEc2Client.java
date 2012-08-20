54775e97d77f852899eda516f85e5526c4b2a70f
Charles Munger <charleslmunger@gmail.com>
2012-08-20 03:44:31 -0700

    Cleaned up imports and removed junk comments.

--- src/clients/PageRankEc2Client.java
+++ src/clients/PageRankEc2Client.java
@@ -1,25 +1,13 @@
 package clients;
 
-import api.MasterOutputMaker;
-import api.MasterGraphMaker;
-import api.WorkerGraphMaker;
-import api.WorkerOutputMaker;
-import system.MasterGraphMakerG1;
-import system.MasterOutputMakerStandard;
 import system.*;
-import system.VertexPageRank;
-import system.WorkerGraphMakerStandard;
-import system.WorkerOutputMakerStandard;
 
 public class PageRankEc2Client extends Client
 {
     public static void main( String[] args ) throws Exception
     {
         int numParts   = Integer.parseInt( args[1] );
-        int numWorkers = Integer.parseInt( args[2] );
-//        String imageIdMaster = args[4] ; 
-//        String imageIdWorker = args[5] ; 
-        
+        int numWorkers = Integer.parseInt( args[2] );        
         Job job = new Job( 
                 "PageRank",
                 args[0],    // jobDirectoryName, 
