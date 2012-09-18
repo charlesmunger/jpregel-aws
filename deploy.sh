31d659be57129e52a23fc9f9765d0c46a4e5ef0b
EC2 Default User <ec2-user@ip-10-245-187-121.ec2.internal>
2012-09-18 00:28:01 +0000

    Fixed deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 cd Client-Resources
 unzip apache-ant-1.8.4-bin.zip
-export ANT_HOME=/home/ec2-user/jpregel-aws/Client-Resources/apache-ant-1.8.4-bin #This will point to the new version
-export PATH=$PATH:/home/ec2-user/jpregel-aws/Client-Resources/apache-ant-1.8.4-bin
-echo export ANT_HOME=/home/ec2-user/jpregel-aws/Client-Resources/apache-ant-1.8.4-bin >> ~/.bashrc
-echo export PATH=$PATH:/home/ec2-user/jpregel-aws/Client-Resources/apache-ant-1.8.4-bin >> ~/.bashrc
+echo export ANT_HOME=/home/ec2-user/jpregel-aws/Client-Resources/apache-ant-1.8.4 >> ~/.bashrc
+echo export PATH=$PATH:/home/ec2-user/jpregel-aws/Client-Resources/apache-ant-1.8.4/bin >> ~/.bashrc
+sudo yum install java-1.6.0-openjdk-devel
+exec bash
