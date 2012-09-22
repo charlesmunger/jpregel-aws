ea1adff6731034686e599d5021bf2c15deaee784
Pete Cappello <cappello@cs.ucsb.edu>
2012-09-21 17:06:12 -0700

    MessageQ: Removed synchronization. Reduced 1000 node ShortestPath from 233 seconds to 54 seconds. Leaving in old code - commented out; maybe my testing was insufficiently thorough.

--- src/system/MessageQ.java
+++ src/system/MessageQ.java
@@ -15,22 +15,50 @@ final public class MessageQ<VertexIdType, MessageValueType>
     MessageQ( Combiner<VertexIdType, MessageValueType> combiner ) { this.combiner = combiner; } 
     
     @Override
-    synchronized public boolean add( Message<VertexIdType, MessageValueType> message )
+//    synchronized public boolean add( Message<VertexIdType, MessageValueType> message )
+//    {
+//        if ( combiner != null && ! isEmpty() ) 
+//        {
+//            message = combiner.combine( remove(), message );
+//            assert isEmpty() : size();
+//        }
+//        return super.add( message );
+//    }
+    public boolean add( Message<VertexIdType, MessageValueType> message )
     {
-        if ( combiner != null && ! isEmpty() ) 
+        if ( combiner != null ) 
         {
-            message = combiner.combine( remove(), message );
-            assert isEmpty() : size();
+            Message polled = poll();
+            if ( polled != null )
+            {
+                assert size() == 1 : size();
+                message = combiner.combine( polled, message ); 
+            }
         }
         return super.add( message );
     }
     
-    synchronized public boolean addAll( MessageQ<VertexIdType, MessageValueType> messageQ )
+//    synchronized public boolean addAll( MessageQ<VertexIdType, MessageValueType> messageQ )
+//    {
+//        if ( combiner != null && ! isEmpty() ) 
+//        {
+//            assert messageQ.size() == 1 : messageQ.size();
+//            return super.add( combiner.combine( remove(), messageQ.remove() ) );
+//        }
+//        assert combiner == null || ( isEmpty() && messageQ.size() == 1 );
+//        return super.addAll( messageQ );
+//    }
+    
+    public boolean addAll( MessageQ<VertexIdType, MessageValueType> messageQ )
     {
-        if ( combiner != null && ! isEmpty() ) 
+        if ( combiner != null ) 
         {
-            assert messageQ.size() == 1 : messageQ.size();
-            return super.add( combiner.combine( remove(), messageQ.remove() ) );
+            Message polled = poll();
+            if ( polled != null )
+            {
+                assert messageQ.size() == 1 : messageQ.size();
+                return super.add( combiner.combine( polled, messageQ.remove() ) );
+            }
         }
         assert combiner == null || ( isEmpty() && messageQ.size() == 1 );
         return super.addAll( messageQ );
