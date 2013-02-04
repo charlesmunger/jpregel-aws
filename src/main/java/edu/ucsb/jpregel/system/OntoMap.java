d24629b49776686057c041f34d295d79b37a76e2
Charles Munger <charleslmunger@gmail.com>
2013-02-03 21:59:29 -0800

    Use thread-local variables to save unnecessary object initialization.

--- src/main/java/edu/ucsb/jpregel/system/OntoMap.java
+++ src/main/java/edu/ucsb/jpregel/system/OntoMap.java
@@ -4,93 +4,84 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
- * A map that creates objects on demand as they are requested. 
- * Uses Longs as keys, and does not create value objects unless their keys do 
- * not already have a corresponding value in the map. 
- * 
- * If multiple threads get(Long key) the same key, one thread will create a 
+ * A map that creates objects on demand as they are requested. Uses Longs as
+ * keys, and does not create value objects unless their keys do not already have
+ * a corresponding value in the map.
+ *
+ * If multiple threads get(Long key) the same key, one thread will create a
  * value object and the others will block until the object is created, and then
  * they will get a reference to it. Because this class is final, this is nothing
- * to worry about. 
+ * to worry about.
  *
  * @author Charles Munger
  */
-final public class OntoMap<V>
-{
+final public class OntoMap<V> {
 
-    private final Factory<V> factory;
-    private final ConcurrentMap<Long, Holder<V>> map;
+	private static final ThreadLocal< Holder> uniqueNum = new ThreadLocal<Holder>() {
 
-    public OntoMap(Factory<V> factory)
-    {
-        this(100, factory);
-    }
+		@Override
+		protected Holder initialValue() {
+			return new Holder();
+		}
+	};
+	private final Factory<V> factory;
+	private final ConcurrentMap<Long, Holder<V>> map;
 
-    public OntoMap(int size, Factory<V> factory)
-    {
-        map = new ConcurrentHashMap<Long, Holder<V>>(100, 0.9f, 2);
-        this.factory = factory;
-    }
+	public OntoMap(Factory<V> factory) {
+		this(100, factory);
+	}
 
-    public V get(final Long key)
-    {
-        final Holder<V> h = new Holder<V>();
-        final Holder<V> putIfAbsent = map.putIfAbsent(key, h);
-        if (putIfAbsent != null)
-        {
-            return putIfAbsent.blockingGet();
-        } else
-        {
-            final V make = factory.make();
-            h.fill(make);
-            return make;
-        }
-    }
+	public OntoMap(int size, Factory<V> factory) {
+		map = new ConcurrentHashMap<Long, Holder<V>>(100, 0.9f, 2);
+		this.factory = factory;
+	}
 
-    public V remove(Long key)
-    {
-        final Holder<V> remove = map.remove(key);
-        return remove == null ? null : remove.get();
-    }
+	public V get(final Long key) {
+		final Holder<V> h = uniqueNum.get();
+		final Holder<V> putIfAbsent = map.putIfAbsent(key, h);
+		if (putIfAbsent != null) {
+			return putIfAbsent.blockingGet();
+		} else {
+			final V make = factory.make();
+			h.fill(make);
+			uniqueNum.set(new Holder());
+			return make;
+		}
+	}
 
-    private class Holder<T>
-    {
+	public V remove(Long key) {
+		final Holder<V> remove = map.remove(key);
+		return remove == null ? null : remove.get();
+	}
 
-        private volatile T contents = null;
+	private static class Holder<T> {
 
-        void fill(T contents)
-        {
-            this.contents = contents;
-            synchronized (this)
-            {
-                notifyAll();
-            }
-        }
+		private volatile T contents = null;
 
-        T blockingGet()
-        {
-            if (contents == null)
-            {
-                synchronized (this)
-                {
-                    try
-                    {
-                        while (contents == null)
-                        {
-                            wait();
-                        }
-                    } catch (InterruptedException ex)
-                    {
-                        System.out.println("Wait interrupted in get");
-                    }
-                }
-            }
-            return contents;
-        }
+		void fill(T contents) {
+			this.contents = contents;
+			synchronized (this) {
+				notifyAll();
+			}
+		}
 
-        T get()
-        {
-            return contents;
-        }
-    }
+		T blockingGet() {
+			if (contents == null) {
+				synchronized (this) {
+					try {
+						while (contents == null) {
+							wait();
+						}
+					} catch (InterruptedException ex) {
+						System.out.println("Wait interrupted in get");
+					}
+				}
+			}
+			return contents;
+		}
+
+		T get() {
+			return contents;
+		}
+	}
 }
